419275569d63928f1b29532eab235939cee0654d
Your Name <jpark1917@gmail.comecho>
2021-10-21 12:39:20 -0700

    line with error

--- code/01-Reshape.R
+++ code/01-Reshape.R
@@ -33,6 +33,8 @@ raw$day <- as.numeric(as.character(raw$day))
 raw <- raw[, c("year", "month", "day", "element", "value")]
 # ------------------------------------------
 
+raw <- 1 + raw # line that causes an error
+ 
 head(raw)
 
 tidy <- dcast(raw, year + month + day ~ element, 
